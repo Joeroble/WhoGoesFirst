216cab5beac8d4666d3210580ac7a65d5f214c84
JRoble <89475216+Joeroble@users.noreply.github.com>
2022-01-28 08:33:51 -0600

    Added in comments for everything.

--- app/src/main/java/com/bignerdranch/android/whogoesfirst/MainActivity.kt
+++ app/src/main/java/com/bignerdranch/android/whogoesfirst/MainActivity.kt
@@ -13,6 +13,8 @@ import kotlin.random.Random.Default.nextBoolean
 class MainActivity : AppCompatActivity() {
 
     // button, two edit texts, one textview
+
+    // Sets up the variables and their types that will be initialized in onCreate
     private lateinit var team1EditText: EditText
     private lateinit var team2EditText: EditText
     private lateinit var resultTextView: TextView
@@ -22,11 +24,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // Initializes the variables setup prior to onCreate and associating them with their
+        // respective entities in the UI
         team1EditText = findViewById(R.id.team_1_edit_text)
         team2EditText = findViewById(R.id.team_2_edit_text)
         resultTextView = findViewById(R.id.result_text_view)
         chooseTeamButton = findViewById(R.id.choose_team_button)
 
+
+        // setsup the onClickListenter for chooseTeamButton which calls to chooseRandomTeam
         chooseTeamButton.setOnClickListener{
             chooseRandomTeam()
         }
@@ -36,13 +42,18 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun chooseRandomTeam() {
-
+        // Saves the text entered in team1 and 2 EditText into their respective variables
         val team1 = team1EditText.text
         val team2 = team2EditText.text
 
 
 
         // options - put in a list and use random
+        // Checks if both team1 and team2 are not blank before going on to picking a random team,
+        // if the check fails, it will print display a message asking the user to fill in both
+        // teams. After the check it establishes a variable that will contain a random boolean, this is
+        // then checked in an if/else block that will change the resultTextView based on if
+        // team1 was selected(teamGoesFirst = true) or if team2 was selected (false/else).
         if(team1.isNotBlank() && team2.isNotBlank()) {
             val teamOneGoesFirst = Random.nextBoolean()
             if (teamOneGoesFirst) {
